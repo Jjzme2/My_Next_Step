a305ad86456e6a19bf7515b2d85b35c78cd3a02e
Jj <Jjzettler@gmail.com>
2024-11-21 10:02:40 -0600

    Refactor views and remove unnecessary files

--- application/server/routes/devRoutes.js
+++ application/server/routes/devRoutes.js
@@ -7,7 +7,7 @@ const NoteCollection = require("../assets/notes/index.js")
 const { renderMarkdown } = require("../utils/markdownRenderer.js");
 
 router.get("/", (req, res) => {
-	  res.render("login", {
+	  res.render("pages/login", {
 	title: "Wiki Home Page", // Dynamic title for the page
   });
 });
@@ -25,7 +25,7 @@ router.get("/connect", (req, res) => {
 // * General
 
 router.get("/home", (req, res) => {
-  res.render("home", {
+  res.render("pages/home", {
     title: "Home Page", // Dynamic title for the page
   });
 });
@@ -34,9 +34,15 @@ router.get("/home", (req, res) => {
 // * Notes
 
 router.get("/notes", (req, res) => {
-	  res.render("noteList", {
+	  res.render("notes/list", {
 	title: "Notes Page", // Dynamic title for the page
 	notes: NoteCollection,
+  });
+});
+
+router.get("/notes/create", (req, res) => {
+  res.render("notes/create", {
+    title: "Create a New Note",
   });
 });
 
@@ -44,7 +50,7 @@ router.get("/notes/:id", (req, res) => {
   const note = NoteCollection.find((note) => note.metadata.id === req.params.id);
 
   if (!note) {
-    return res.status(404).render("404", {
+    return res.status(404).render("general/404", {
       title: "Page Not Found",
       error: "Note not found",
       additionalInfo: req.params.id,
@@ -53,15 +59,9 @@ router.get("/notes/:id", (req, res) => {
 
   const renderedContent = renderMarkdown(note.content);
 
-  res.render("note", {
+  res.render("notes/view", {
     title: "Wiki - " + note.metadata.title, // Dynamic title for the page
     content: renderedContent, // Pass content to the template
-  });
-});
-
-router.get("/notes/create", (req, res) => {
-  res.render("newNote", {
-    title: "Create a New Note",
   });
 });
 
@@ -99,7 +99,7 @@ router.get("/resources", (req, res) => {
 	const resources = require("../assets/storage/resources.js");
   const packages = require("../assets/storage/packages.js");
 
-	  res.render("resources", {
+	  res.render("pages/resources", {
 	title: "Resources Page", // Dynamic title for the page
 	resources: resources,
   packages: packages,
